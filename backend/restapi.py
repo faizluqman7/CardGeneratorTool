100a6f85f6bcb15161d1c5ebf495cefe3bbf96b5
faizluqman7 <faizluqman7@gmail.com>
2025-03-14 17:14:55 +0000

    Render

--- backend/restapi.py
+++ backend/restapi.py
@@ -43,4 +43,5 @@ def download_pdf():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
